36dac8b1bb9b8e5c4b8d588ee90fcc605254d02d
Angel Aviel Domaoan <amdomaoan@cdasia.com>
2019-06-04 15:52:44 +0800

    Add `search_params`

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   def index
-    search = Document.search_for(params[:q] || "*")
+    search = Document.search_for(search_params[:q] || "*")
     @documents = search.records
+  end
+
+  private
+
+  def search_params
+    params.permit(:q)
   end
 end
