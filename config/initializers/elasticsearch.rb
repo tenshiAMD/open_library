077add5a6fcd0b7e4b52d4154762d33f331ab6de
Angel Aviel Domaoan <amdomaoan@cdasia.com>
2019-06-13 14:24:54 +0800

    Fix elasticsearch healthcheck

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,4 @@
 ENV["ELASTICSEARCH_URL"] ||= Rails.application.credentials.elasticsearch_url
+
+OkComputer::Registry.register "elasticsearch",
+                              OkComputer::ElasticsearchCheck.new(ENV["ELASTICSEARCH_URL"]) # rubocop:disable Metrics/LineLength
--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -7,10 +7,6 @@ OkComputer::OkComputerController.class_eval do
   force_ssl except: :index if ENV["STATS_SSL"] == true
 end
 
-ELASTICSEARCH_URL = Rails.application.credentials.dig(:elasticsearch_url)
-OkComputer::Registry.register "elasticsearch",
-                              OkComputer::ElasticsearchCheck.new(ELASTICSEARCH_URL) # rubocop:disable Metrics/LineLength
-
 if ActionMailer::Base.smtp_settings[:address].present?
   OkComputer::Registry.register "mailer",
                                 OkComputer::ActionMailerCheck.new(ActionMailer::Base) # rubocop:disable Metrics/LineLength
