7d2625dfcefb8dbb3357405dcc7f66327b933fdf
Angel Aviel Domaoan <amdomaoan@cdasia.com>
2019-06-13 14:36:03 +0800

    Tweak healthcheck command

--- docker/app/check-app.sh
+++ docker/app/check-app.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-curl -f -u ${HEALTH_CHECK_AUTH} http://localhost:80/health/all || exit 1
+if [[ -z "${HEALTH_CHECK_AUTH}" ]]; then
+  curl -f -u ${HEALTH_CHECK_AUTH} http://localhost:80/health/all || exit 1
+else
+  exit 0
+fi
